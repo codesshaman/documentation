a81801a92293f38620fe4c3e180c253cefc2561e
Nick Vasil'ev <90702473+codesshaman@users.noreply.github.com>
2024-11-27 18:47:43 +0300

    Create push.sh

---
+++ git/push.sh
@@ -0,0 +1,25 @@
+# Выводим git status
+
+git status
+# Запрашиваем пользовательский ввод для выполнения пуша
+read -p "Хотите выполнить пуш? (y/n): " answer
+
+# Если пользователь ввел "y" или "Y", продолжаем выполнение
+if [[ $answer == "y" || $answer == "Y" ]]; then
+    # Выполняем git add .
+    git add .
+
+    # Выводим git status после git add .
+    git status
+
+    # Запрашиваем текст коммита от пользователя
+    read -p "Введите коммит: " commit_message
+
+    # Выполняем git commit с введенным пользовательским сообщением
+    git commit -m "$commit_message"
+
+    # Выполняем git push
+    git push
+fi
+
+git status
